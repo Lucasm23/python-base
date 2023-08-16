d28def356903394d47337793e8452724170a942e
root <root@DESKTOP-UNHV9B2>
2023-08-16 17:25:56 -0300

    Ola Mundo em varios idiomas

--- hello.py
+++ hello.py
@@ -1,1 +1,25 @@
-print("Hello,World")
+#!/usr/bin/env python3
+
+
+__version__ = "0.0.1"
+__author__ = "Lucas Mendes"
+__license__ = "Unlicense"
+
+import os
+
+
+
+current_language = os.getenv("LANG","en_US")[:5]
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Munndo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
+
+print(msg)
+
